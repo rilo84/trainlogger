14288bec19e6c8da2781614a3f0074c9c0e51582
Richard Lo Sinno <richard.losinno@gmail.com>
2026-08-31 18:40:08 +0200

    Revert "init fitnessSync integration"
    This reverts commit 373e20e6361a4a0b7af4c0bc9f4e0b4dd8004b2d.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n/i18n'
 import App from './App.tsx'
-import { ActivityStreamNotifications } from './components/ActivityStreamNotifications.tsx'
 import { PwaUpdatePrompt } from './components/PwaUpdatePrompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <PwaUpdatePrompt />
-    <ActivityStreamNotifications />
   </StrictMode>,
 )
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/react" />
-
-interface ImportMetaEnv {
-  /** Base URL of the Treni backend. Defaults to http://localhost:3000 in dev. */
-  readonly VITE_API_BASE_URL?: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
